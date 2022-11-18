335d39efdc73c6ed9dd353dcf15b2a11655cb0f6
manivasagam <s.kishore123.64@gmail.com>
2022-11-18 17:13:15 +0530

    ...

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const config ={
-    // api : "https://nov7th.herokuapp.com",
-    api : "http://localhost:3003"
+    api : "https://nov7th.herokuapp.com",
+    // api : "http://localhost:3003"
 }
